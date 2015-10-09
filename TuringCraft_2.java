5150adc26532cb7eafffcd250d1dffb30c80e571
Alex Singleton <alexanderjsingleton@gmail.com>
2015-10-09 17:20:02 -0500

    logical and relational intro for Java

---
+++ TuringCraft_2.java
@@ -0,0 +1,86 @@
+// Given  that the variables  x and y have already  been declared  and assigned  values , write an expression  that evaluates  to true  if x is non-negative and y is negative.
+
+
+// Instructor Notes:
+// Here is the solution for you to enter.  Since an expression is asked for, no ";" is required.  "&&" is the Java syntax for the logical operator and
+// x>=0 && y<0
+
+x>=0 && y<0
+
+// Given  two variables , isEmpty of type  boolean , indicating whether  a class  roster is empty or not, and numberOfCredits of type  int , containing the number of credits for a class , write an expression  that evaluates  to true  if the class  roster is empty or the class  is exactly three credits.
+
+
+// Instructor Notes:
+// Here is solution for you to enter.  In java "||" is the logical operator or.  No ";" since an expression.
+//                    isEmpty || numberOfCredits==3
+// Note isEmpty is a simpler way to test if the variable isEmpty has a true value.
+// Instead of just isEmpty try the expression isEmpty == true and you should also a correct answer
+// Also experiment by using "=" instead of "==" and note the result
+
+isEmpty || numberOfCredits==3
+
+// Write an expression  that evaluates  to true  if and only if the value  of the integer  variable  x is equal  to zero.
+
+x == 0
+
+// Write an expression  that evaluates  to true  if and only if the variables  profits and losses are exactly equal .
+
+
+// Instructor Notes:
+// Recall that in Java the logical operator that tests for equality is ==
+
+profits == losses
+
+// Write an expression  that evaluates  to true  if the value  of index is greater than  the value  of lastIndex.
+
+index > lastIndex
+
+// Working overtime is defined as having worked more than 40 hours during the week. Given  the variable  hoursWorked, write an expression  that evaluates  to true  if the employee worked overtime.
+
+hoursWorked > 40
+
+
+// Given  the variables  numberOfMen and numberOfWomen, write an expression  that evaluates  to true  if the number of men is greater than  or equal  to the number of women.
+numberOfMen >= numberOfWomen
+
+// Given  a double  variable  called average, write an expression  that is true  if and only if the variable 's value  is less than  60.0.
+
+average < 60.0
+
+// Given  an int  variable  grossPay, write an expression  that evaluates  to true  if and only if the value  of grossPay is less than  10,000.
+
+grossPay < 10000
+
+// Given  the variables  temperature and humidity, write an expression  that evaluates  to true  if and only if the temperature is greater than  90 and the humidity is less than  10.
+
+
+// Instructor Notes:
+// Recall that in Java the logical operator and is &&
+
+temperature > 90 && humidity < 10
+
+
+// Given  the integer  variables  yearsWithCompany and department, write an expression  that evaluates  to true  if yearsWithCompany is less than  5 and department is not equal  to 99.
+
+
+// Instructor Notes:
+// Recall that in Java the logical operator not equal is !=
+
+yearsWithCompany < 5 && department != 99
+
+// Given  that the variables  x and y have already  been declared  and assigned  values , write an expression  that evaluates  to true  if x is positive (including zero) or y is negative.
+
+x >= 0 || y < 0
+
+// Given  the variables  isFullTimeStudent and age, write an expression  that evaluates  to true  if age is less than  19 or isFullTimeStudent is true .
+
+age < 19 || isFullTimeStudent == true
+
+// Write an expression  that evaluates  to true  if and only if value  of the boolean  variable  isAMember is false .
+
+isAMember == false
+
+
+
+
+
